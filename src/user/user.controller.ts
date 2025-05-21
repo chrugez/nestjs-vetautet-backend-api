3e0324a52c9f4545d6df81c61ad87e8cebda816d
chrugez <thanhcuis99@gmail.com>
2025-05-21 21:59:30 +0700

    feat: Upload file use Multer - DiskStorage

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,16 +6,47 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from './oss';
+import * as path from 'path';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  // for upload avatar
+  @Post('upload/avatar')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads/avatar',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 3,
+      },
+      fileFilter(req, file, cb) {
+        //extName
+        const extName = path.extname(file.originalname);
+        if (['.jpg', '.png', '.gif'].includes(extName)) {
+          cb(null, true);
+        } else {
+          cb(new BadRequestException('Upload file error'), false);
+        }
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('upload file: ', file.path);
+    return file.path;
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
